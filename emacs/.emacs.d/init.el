bc2e123e191d3f68a5e7d4310523e9209658f054
Mitch Tishmack <mitch.tishmack@gmail.com>
2015-02-28 12:52:54 -0600

    use-package cleanup and speedup.
    Made init.el be.. stupid simple. And with :defer and other shenanigans,
    speedup the overal startup of emacs.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,25 +1,5 @@
 ;;-*-mode: emacs-lisp; coding: utf-8;-*-
-;; Shamelesly copied from https://github.com/correl/dotfiles
-;; Bootstrap us into being able to use org for the initialization file.
-(require 'package)
-(package-initialize)
-
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-;; Install use-package for later use and to install org mode.
-(unless (package-installed-p 'use-package)
-  (progn
-    (package-refresh-contents)
-    (package-install 'use-package)
-    (package-initialize)))
-
-(require 'use-package)
-
-;; Somewhat needed so we can use org to manage things.
-(use-package org
-  :ensure org-plus-contrib)
-
+;; I'm normally just using recent emacs here, so a basic org mode is
+;; already installed. Makes this even simpler.
 (require 'org)
 (org-babel-load-file "~/.emacs.d/emacs.org")
