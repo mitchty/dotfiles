a2b1047560cc7a90fe48f74c2bb722b80d9b7a83
Mitch Tishmack <mitch.tishmack@gmail.com>
2016-03-28 17:10:49 -0500

    checkpoint

--- haskell/.ghc.hs
+++
@@ -1,8 +0,0 @@
-import Control.Applicative
-import Control.Monad
-import Data.Monoid
-import Data.Maybe
-import Data.List
-import Data.Bool
-import qualified Data.Set as S
-import qualified Data.Map as M
