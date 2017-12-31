5e2cd382f73d03f59c9c9af8ce9b40eecd8cf5ec
Mitch Tishmack <mitch.tishmack@gmail.com>
2017-12-31 13:24:27 -0600

    options/default-macos.el: Initial commit for macos options
    For sym/hard-linking a hostname link to.

---
+++ options/default-macos.el
@@ -0,0 +1,14 @@
+;;-*-mode: emacs-lisp; coding: utf-8;-*-
+;; File: default-macos.el
+;; Copyright: 2017 Mitch Tishmack
+;; Description: default options for macos
+(setq testing-p t)
+(setq macos-p t)
+(setq nix-p t)
+(setq tmux-p t)
+(setq git-p t)
+(setq emacs-p t)
+(setq zsh-p t)
+(setq mosh-p t)
+(setq haskell-p t)
+(setq perl-p t)
