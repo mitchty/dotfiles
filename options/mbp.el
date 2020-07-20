e13deeba3b7b18c04925ef2d11f103158284fe44
Mitch Tishmack <mtishmack@cray.com>
2020-07-20 11:51:16 -0500

    I need to update changes more often.

---
+++ options/mbp.el
@@ -0,0 +1,13 @@
+;;-*-mode: emacs-lisp; coding: utf-8;-*-
+;; File: mbp.el
+;; Copyright: 2017 Mitch Tishmack
+;; Description: Options for mbp laptop
+;;(setq testing t)
+(setq macos-p t)
+(setq nix-p t)
+(setq tmux-p t)
+(setq git-p t)
+(setq emacs-p t)
+(setq zsh-p t)
+(setq mosh-p t)
+(setq perl-p t)
